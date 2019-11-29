ffb42d7d885d6b2c1f29d660c2943d95db5d43d2
NguyenThanhDong <nguyenthanhdong1611@gmail.com>
2019-11-29 15:34:20 +0700

    add display result of stackoverflow

--- ts/stackoverflow.ts
+++ ts/stackoverflow.ts
@@ -1,13 +1,15 @@
 import Request from './libs/request'
 import Message from './libs/message'
 
+import * as _ from 'lodash'
+
 
 export default class Stackoverflow {
 
     static order: string = "desc"
     static sort: string = "activity"
     static site: string = "stackoverflow"
-    static pagesize: number = 2
+    static pagesize: number = 4
 
     static async search(text: string) {
         const url = `https://api.stackexchange.com/2.2/search?order=${Stackoverflow.order}&sort=${Stackoverflow.sort}&intitle=${text}&site=${Stackoverflow.site}&pagesize=${Stackoverflow.pagesize}`
@@ -22,7 +24,7 @@ export default class Stackoverflow {
         } = await Request.get(url)
 
         if (!results.success) {
-            return {success: false, message: Message.error(500)}
+            return {success: false, message: results.err}
         }
 
         if (!results.data.items) {
@@ -30,5 +32,25 @@ export default class Stackoverflow {
         }
 
         return {success: true, data: results.data.items}
+    }
+
+    static show(results = []): string {
+        let message = ""
+        if (results.length == 0) {
+            message = "Sorry, I couldn't find any matches"
+        }
+        for (let item in results) {
+            const tags = results[item].tags ? results[item].tags.join(", ") : ""
+            if (tags) {
+                message += `\`Tags\`: ${tags}\n`
+            }
+
+            message += `\`Title\`: ${_.startCase(results[item].title)}\n`
+            message += `\`Link\`: ${results[item].link}\n`
+            message += `\`Reputation\`: ${results[item].owner.reputation}\n`
+            message += `\`View count\`: ${results[item].view_count}\n`
+            message += "\n\n\n"
+        }
+        return message
     }
 }
