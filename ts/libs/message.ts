ff57d6595cb5b084cb5c0f8354ede89175055673
NguyenThanhDong <nguyenthanhdong1611@gmail.com>
2019-11-29 15:33:51 +0700

    add message

--- ts/libs/message.ts
+++ ts/libs/message.ts
@@ -1,5 +1,8 @@
 const TEXTS = {
 
+    100: "Welcome to Sonic Bot. Currently we only support searching on stackoverflow",
+    101: "Please enter text and waitting results",
+    102: "Sorry, search result not success, please try again",
     401: "Unauthorization",
     500: "An error occurred during processing, please try again later"
 }
